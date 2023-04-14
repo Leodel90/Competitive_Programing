90f7ad3b19e1c2ad21271588db048517ad061f2d
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2023-04-13 23:05:47 -0400

    1382

--- Leetcode/1382 - Balance a Binary Search Tree.java
+++ Leetcode/1382 - Balance a Binary Search Tree.java
@@ -1,34 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int M [][] = new int [n+1][m+1];
-        for (int i = 1; i<=n; i++) {
-            for (int j = 1; j<=m; j++) {
-                M[i][j] = matrix[i-1][j-1];
-            }
-        }
-        int c = 0;
-        matrix = null;
-        for (int i=1; i<=n; i++) {
-            HashMap<Integer, Integer> hash []= new HashMap[i+1];
-            for (int j=1; j<=i; j++) {
-                hash[j] = new HashMap<>();
-            }
-            
-            for (int j=1; j<=m; j++) {
-                M[i][j] = M[i][j] + M[i-1][j] + M[i][j-1] - M[i-1][j-1];
-                for (int k = 1; k<=i; k++) {
-                    int diff = M[i][j] - M[k-1][j];
-                    if (diff == target) { c++; }
-                    if (hash[k].containsKey(diff-target)) {
-                        c += hash[k].get(diff-target);
-                    }
-                    hash[k].put(diff, hash[k].containsKey(diff)? hash[k].get(diff)+1:1);
-                }
-            }
-        }
-        
-        return c;
+    Integer nodes[];
+    List<Integer> nodesL;
+    public void inOrder(TreeNode node){
+        if(node==null) return;
+        inOrder(node.left);
+        nodesL.add(node.val);
+        inOrder(node.right);
+    }
+    public TreeNode balancedBST (int l, int r){
+        if(r<l){return null;}
+        if(l==r){return new TreeNode(nodes[l]);}
+        int m = (l+r)>>1;
+        return new TreeNode(nodes[m], balancedBST(l, m-1), balancedBST(m+1,r));
+    }
+    public TreeNode balanceBST(TreeNode root) {
+        nodesL = new LinkedList<>();
+        inOrder(root);
+        nodes = nodesL.toArray(new Integer[0]);
+        return balancedBST(0, nodes.length-1);
     }
 }
